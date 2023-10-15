970a3a442568b275295dfa301d7c50edc5251275
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2023-10-16 00:50:53 +0100

    Sumneko Utils Updates - WIP 1

--- control.lua
+++ control.lua
@@ -2,7 +2,7 @@ local function CreateGlobals()
 end
 
 local function OnLoad()
-	--Any Remote Interface registration calls can go in here or in root of control.lua
+    --Any Remote Interface registration calls can go in here or in root of control.lua
 end
 
 ---@param event EventData.on_runtime_mod_setting_changed|nil # nil value when called from OnStartup (on_init & on_configuration_changed)
@@ -14,7 +14,7 @@ end
 
 local function OnStartup()
     CreateGlobals()
-	OnLoad()
+    OnLoad()
     OnSettingChanged(nil)
 end
 
