19576e45f090121323c6fa5191c7f1ed8c05c621
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2023-11-02 00:13:08 +0000

    Move prototype like lists to Utils
    Need the logic in another mod so will leave a TODO there to move to the Utility version in future.

--- data-final-fixes/apply-zombie-path-collision-mask.lua
+++ data-final-fixes/apply-zombie-path-collision-mask.lua
@@ -1,5 +1,5 @@
-local TableUtils = require("utility.helper-utils.table-utils")
 local CollisionMaskUtil = require("__core__/lualib/collision-mask-util")
+local PrototypeLikeLists = require("utility.helper-utils.prototype-like-lists-data-stage")
 
 local zombieEngineerPathCollisionLayer = data.raw["simple-entity"]["zombie_engineer-zombie_engineer_path_collision_layer"].collision_mask[1]
 
@@ -9,64 +9,36 @@ local function AddZombiePathLayerToPrototype(prototype)
     prototype.collision_mask = newMask
 end
 
---------------------------------------
---           Biters
---------------------------------------
-
--- Add to Biter Worms. This is a best guess as in vanilla the only plain turrets are worms.
-for _, prototype in pairs(data.raw["turret"] --[[@as data.TurretPrototype[] ]]) do
-    if prototype.subgroup == "enemies" then
-        AddZombiePathLayerToPrototype(prototype)
-    end
+-- Biters / Enemies
+for _, prototype in pairs(PrototypeLikeLists.GetWormTurretLikePrototypes()) do
+    AddZombiePathLayerToPrototype(prototype)
+end
+for _, prototype in pairs(PrototypeLikeLists.GetEnemySpawnerLikePrototypes()) do
+    AddZombiePathLayerToPrototype(prototype)
 end
 
--- Add to Biter Spawners. This is a best guess.
-for _, prototype in pairs(data.raw["unit-spawner"] --[[@as data.EnemySpawnerPrototype[] ]]) do
-    if prototype.subgroup == "enemies" then
-        AddZombiePathLayerToPrototype(prototype)
-    end
+
+-- Natural Environment.
+for _, prototype in pairs(PrototypeLikeLists.GetMinableRockLikePrototypes()) do
+    AddZombiePathLayerToPrototype(prototype)
 end
-
---------------------------------------
---           Natural Environment.
---------------------------------------
-
--- Add to rocks and similar things.
-for _, prototype in pairs(data.raw["simple-entity"] --[[@as data.SimpleEntityPrototype[] ]]) do
-    if prototype.count_as_rock_for_filtered_deconstruction == true then
-        AddZombiePathLayerToPrototype(prototype)
-    end
-end
-
--- Add to all trees rocks. Best guess.
 for _, prototype in pairs(data.raw["tree"] --[[@as data.TreePrototype[] ]]) do
     AddZombiePathLayerToPrototype(prototype)
 end
 
---------------------------------------
---           Tiles
---------------------------------------
-
--- Water types. Best guess. Need to exclude the shallow waters, hence check of collision masks.
-for _, prototype in pairs(data.raw["tile"] --[[@as data.TilePrototype[] ]]) do
-    if prototype.draw_in_water_layer == true and TableUtils.GetTableKeyWithValue(prototype.collision_mask, "player-layer", false) ~= nil then
-        local x = TableUtils.GetTableKeyWithValue(prototype.collision_mask, "player-layer", false)
-        AddZombiePathLayerToPrototype(prototype)
-    end
+-- Tiles
+for _, prototype in pairs(PrototypeLikeLists.GetBlockingWaterTileLikePrototypes()) do
+    AddZombiePathLayerToPrototype(prototype)
 end
-
--- Void types.
-for _, prototype in pairs(data.raw["tile"] --[[@as data.TilePrototype[] ]]) do
-    if prototype.layer_group == "zero" then
-        AddZombiePathLayerToPrototype(prototype)
-    end
+for _, prototype in pairs(PrototypeLikeLists.GetVoidTileLikePrototypes()) do
+    AddZombiePathLayerToPrototype(prototype)
 end
 
 --------------------------------------
 --           Other Muppet Mods
 --------------------------------------
 
--- Egg nests. This catches them all currently and avoids hard coding which is nice.
+-- Egg nests. This catches them all currently and avoids hard coding which is nice. Isn't terribly inter mod compatible mind.
 for _, prototype in pairs(data.raw["simple-entity-with-force"] --[[@as data.SimpleEntityWithForcePrototype[] ]]) do
     if prototype.subgroup == "enemies" then
         AddZombiePathLayerToPrototype(prototype)
