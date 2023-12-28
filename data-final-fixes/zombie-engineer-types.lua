92da89a4c08aa3652f59dd6eca5a4089b1369362
Muppet9010 <mike.hollett@gmail.com>
2023-12-28 21:26:32 +0000

    test and fix scaling zombie health stats
    updated spreadsheet logic to try and re-balance the later armors better.

--- data-final-fixes/zombie-engineer-types.lua
+++ data-final-fixes/zombie-engineer-types.lua
@@ -1,11 +1,23 @@
 -- Make variations of the zombie engineer prototype or each armor type in the game.
 -- Armors below heavy armor are nerfed significantly by this, but tbh heavy armor and after is the majority of the game and so getting these balanced is what matters most.
+-- The defensive values on all armors are shifted downwards to try and find some type of a balance across armors and weapons (with some vague assumed damage upgrades to balance out things later on).
 -- This implements the details as per spreadsheet (scaling damage); https://docs.google.com/spreadsheets/d/1yIMR8AXHv2wZXUDJXdqdJD0vWZkzh4r-hTw64ydeHQ0/edit#gid=1004830454
+-- This is designed around vanilla weapons and armors. I'm still not entirely happy with it, but its better than older simpler approaches.
 
 local TableUtils = require('utility.helper-utils.table-utils')
 local PrototypeUtils = require("utility.helper-utils.prototype-utils-data-stage")
 
 local characterPrototypeReference = data.raw["character"]["character"]
+
+local modifiers = {
+    difficultyExplosionReductionFlatMultiplier = 0.1,
+    difficultyTotalReductionFlatMultiplier = 0.6,
+    difficultyFinalModifier = -5,
+    resistancePhysicalReductionFlatModifier = -4,
+    resistancePhysicalReductionPercentModifier = -20,
+    resistanceExplosionReductionFlatModifier = -20,
+    resistanceExplosionReductionPercentModifier = -30
+}
 
 
 ---@param armorPrototype data.ArmorPrototype
@@ -26,9 +38,9 @@ local GetZombieDifficultyForArmor = function(armorPrototype)
         local explosionReductionFlat = (explosionResistance ~= nil and explosionResistance.decrease ~= nil) and explosionResistance.decrease or 0
         local physicalResistance = TableUtils.GetTableValueWithInnerKeyValue(armorResistances, "type", "physical", false) --[[@as data.Resistance?]]
         local physicalReductionFlat = (physicalResistance ~= nil and physicalResistance.decrease ~= nil) and physicalResistance.decrease or 0
-        local totalReductionFlat = (explosionReductionFlat * 0.1) + physicalReductionFlat
+        local totalReductionFlat = ((explosionReductionFlat * modifiers.difficultyExplosionReductionFlatMultiplier) + physicalReductionFlat) * modifiers.difficultyTotalReductionFlatMultiplier
 
-        zombieDifficulty = (totalReductionPercent + totalReductionFlat) - 8
+        zombieDifficulty = (totalReductionPercent + totalReductionFlat) + modifiers.difficultyFinalModifier
     end
 
     return math.max(zombieDifficulty, 1)
@@ -42,8 +54,11 @@ local GetZombieResistancesForArmor = function(armorPrototype)
     local zombieResistances = {} ---@type data.Resistance[]
     for _, resistance in pairs(armorResistances) do
         if resistance.type == "physical" then
-            -- Just reduce the physical resistance to make the yellow ammo vs heavy armor era balanced.
-            resistance.decrease = math.max(resistance.decrease - 4, 0)
+            resistance.decrease = math.max(resistance.decrease + modifiers.resistancePhysicalReductionFlatModifier, 0)
+            resistance.percent = math.max(resistance.percent + modifiers.resistancePhysicalReductionPercentModifier, 0)
+        elseif resistance.type == "explosion" then
+            resistance.decrease = math.max(resistance.decrease + modifiers.resistanceExplosionReductionFlatModifier, 0)
+            resistance.percent = math.max(resistance.percent + modifiers.resistanceExplosionReductionPercentModifier, 0)
         end
         zombieResistances[#zombieResistances + 1] = resistance
     end
